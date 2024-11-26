2d73c49f6637f909d18709bebb811f15a4b04070
r4gtl <rquattrogtl@gmail.com>
2024-11-26 11:54:10 +0100

    OutputLavorazioni

--- tannerySuite/lavorazioni/forms.py
+++ tannerySuite/lavorazioni/forms.py
@@ -1,23 +1,4 @@
 from django import forms
 
-from .models import Lavorazione
+from .models import *
 
-
-class LavorazioneModelForm(forms.ModelForm):
-    class Meta:
-        model = Lavorazione
-        fields = '__all__'
-
-
-        widgets = {
-            'descrizione': forms.TextInput(attrs={'placeholder': 'Inserisci descrizione lavorazione'}),            
-            'codice': forms.TextInput(attrs={'placeholder': 'Inserisci codice lavorazione'}),            
-            'note': forms.Textarea(attrs={'placeholder': 'Inserisci Annotazioni', 'rows':'3'}),
-            'created_by': forms.HiddenInput(),
-            'created_at': forms.HiddenInput()
-        }
-        labels = {
-            'descrizione': 'Descrizione',
-            'codice': 'Codice',
-            'note': 'Annotazioni'
-        }
